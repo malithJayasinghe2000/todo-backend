d510d641a7d21ce65caa9f2d30d59b08a440f321
Malith Jayasinghe <malith2jayasinghe@gmail.com>
2025-04-05 22:18:25 +0530

    add a route

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
 
 ///API endpoints
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-//     });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    });
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
 app.use('/api/task',taskRouter);
